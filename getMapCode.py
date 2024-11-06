f73e1fa725eb726bcaa5e03715bd1185695344c3
pietrolcm <pietrolabacm@gmail.com>
2024-11-06 09:56:35 -0300

    Cleanup

--- getMapCode.py
+++ getMapCode.py
@@ -1,16 +1,35 @@
-from shapely.geometry import Point, shape
+from shapely.geometry import shape, Point
 import fiona
+import os
 
+def getMapCode(lat:float,long:float, scn='scn_100k.gpkg'):
+    '''Returns the map code of a point from given latitude and longitude 
+    according to the Sistema Cartográfico Nacional Brasileiro.
 
-def getMapCode(vector, scn='scn_100k.gpkg'):
-    shapePath = scn
+    :param lat: Latitude
+    :type lat: float
+    :param long: Longitude
+    :type long: float
+    :param scn: Path to the file storing the codes polygons, defaults to 'scn_100k.gpkg'
+    :type scn: str, optional
+    :return: Map code according to the SCNB
+    :rtype: string
+    '''
+    if not os.path.isfile(scn):
+        if scn == 'scn_100k.gpkg':
+            raise FileNotFoundError(
+                "Map Code polygons file not found\nRun 'python createScn.py' to create the files needed to use the getMapCode function")
+        else:
+            raise FileNotFoundError('Map Code polygons file not found\nDoes the file %s exists?'%scn)
+            
+    point = Point(long,lat)
     map_code_list = []
-    with fiona.open(shapePath) as shapefile:
+    with fiona.open(scn) as shapefile:
         for feature in shapefile:
             if not feature.geometry:
                 continue
             featureGeometry = shape(feature.geometry)
-            if vector.within(featureGeometry):
+            if point.within(featureGeometry):
                 map_code = feature.properties.get('code')
                 map_code_list.append(map_code)
         if len(map_code_list) == 1:
--- test.py
+++
@@ -1,8 +0,0 @@
-from getMapCode import getMapCode
-from shapely.geometry import Point
-
-p1 = Point(-39.24,-18.26)
-expected_code = 'SE-24-Y-B'
-
-map_code = getMapCode(p1,'scn_250k.gpkg')
-assert map_code == expected_code
